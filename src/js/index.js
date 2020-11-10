e3dd377990709a663970d5ea1062145c525a9ed6
Dawid Klos <klosdawid12@gmail.com>
2020-11-10 17:17:39 +0000

    Fixed problem with opacity on glass of water image after refresh

--- src/js/index.js
+++ src/js/index.js
@@ -32,30 +32,33 @@ const waterGlass = document.querySelector('.main__water-glass');
 // localStorage values //
 const key = new Date().toISOString().slice(0, 10);
 const keyValue = localStorage.getItem(key);
+const firstTime = localStorage.getItem("firstTime");
+const opacity = localStorage.getItem("opacity");
+
+// Settings options //
+const goalChoice = document.querySelector('.goal--js');
+const capacityChoice = document.querySelector('.capacity--js');
+const saveChoices = document.querySelector('.select--js');
+const goalValue = localStorage.getItem(select1);
+const capacityValue = localStorage.getItem(select2);
+
 
 //             //
 
 
 // GREETINGS NEW USERS //
 
+if (!firstTime) {
+    swal("WELCOME!", "Please, before you start to explore our app:\n Allow for notifications.\n It helps to keep your hydration on 100%! \n And don't forget to install our app! :)", "info");
+    localStorage.setItem("firstTime", "1");
+};
+
+
+// WELCOME TO USERS EVERY DAY //
+
 
 if (!keyValue) {
     swal("WELCOME!", "Nice to see you back. :)", "info");
-}
-
-
-// LOCALSTORAGE //
-
-let result = '';
-counter.value = result;
-
-if (!keyValue) {
-    result = 0;
-    localStorage.setItem(key, result);
-} else {
-    water.style.opacity = 0.05;
-    result = keyValue;
-    counter.innerHTML = result;
 }
 
 // ADDING A GLASS //
@@ -67,7 +70,7 @@ addGlass.addEventListener('click', function (event) {
         swal("", "You've alredy drunk 1l of water.\n \n Good job! ", "success");
     }
     if (result == 16) {
-        swal("", "You have to be very thursty! \n \n 4l behind you :)");
+        swal("", "You have to be very thursty! \n \n 4l behind you :)", "success");
     }
     localStorage.setItem(key, result);
     counter.innerHTML = result;
@@ -102,7 +105,15 @@ function glassAnimation() {
 
 // WATER OPACITY //
 
-let myGoal = 20;
+let opacityValue;
+
+if (!opacity || !keyValue) {
+    water.style.opacity = 0.05;
+    localStorage.setItem("opacity", "0.05");
+} else {
+    opacityValue = opacity;
+    water.style.opacity = opacityValue;
+}
 
 const progress = () => {
     water.style.opacity = `${progress}%`;
@@ -110,23 +121,50 @@ const progress = () => {
   
     if (progress <= 20) {
       water.style.opacity = 0.2;
+      opacityValue = water.style.opacity;
     } else if (progress <= 40) {
       water.style.opacity = 0.3;
+      opacityValue = water.style.opacity;
     } else if (progress <= 60) {
       water.style.opacity = 0.4;
+      opacityValue = water.style.opacity;
     } else if (progress <= 70) {
       water.style.opacity = 0.5;
+      opacityValue = water.style.opacity;
     } else if (progress <= 80) {
       water.style.opacity = 0.6;
+      opacityValue = water.style.opacity;
     } else if (progress <= 90) {
       water.style.opacity = 0.7;
+      opacityValue = water.style.opacity;
     } else {
       water.style.opacity = 1;
+      opacityValue = water.style.opacity;
     }
     if (progress == 100) {
-      swal("Daily goal", "Congratulations! \n You've achieved your goal today :)", "success");
+      swal("Daily goal", "Congratulations! \n \n You've achieved your goal today :)", "success");
     }
+    localStorage.setItem("opacity", opacityValue);
   };
+
+
+
+
+  // LOCALSTORAGE - loading //
+
+let result = '';
+counter.value = result;
+
+if (!keyValue) {
+    result = 0;
+    localStorage.setItem(key, result);
+} else {
+    result = keyValue;
+    counter.innerHTML = result;
+
+}
+
+
 
 
 // HAMBURGER MENU //
@@ -146,6 +184,35 @@ menuSettings.addEventListener('click', function (event) {
     nav.classList.toggle('nav--open');
     hamburger.classList.toggle('is-active');
 });
+
+
+// myGoal and myCapacity //
+
+
+let select1 = 'myGoal';
+let select2 = 'myCapacity';
+let myGoal = 8;
+let myCapacity = 250;
+
+if(!goalValue || !capacityValue) {
+    localStorage.setItem(select1, myGoal);
+    localStorage.setItem(select2, myCapacity);
+} else {
+    myGoal = goalValue;
+    myCapacity = capacityValue;
+}
+
+// Save choice //
+
+saveChoices.addEventListener('click', function (event) {
+    myGoal = goalChoice.value;
+    myCapacity = capacityChoice.value;
+    localStorage.setItem(select1, myGoal);
+    localStorage.setItem(select2, myCapacity);
+
+});
+
+
 
 //   HOME  - subpage   //
 
@@ -180,7 +247,7 @@ const storage = Object.entries(localStorage).sort((a, b) => b[0].localeCompare(a
 
 let ctx = document.getElementById('myChart').getContext('2d');
 let config = {
-    type: 'line',
+    type: 'bar',
     data: {
         labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         datasets: [{
@@ -255,3 +322,28 @@ function updateConfig() {
 
 drinkingChartValues();
 updateDrinkingChart();
+
+
+// PUSH NOTIFICATIONS - test //
+
+// Notification.requestPermission(function(status) {
+//     console.log('Notification permission status:', status);
+// });
+
+// function displayNotification() {
+//     if (Notification.permission == 'granted') {
+//       navigator.serviceWorker.getRegistration().then(function(reg) {
+//         var options = {
+//           body: 'Here is a notification body!',
+//           icon: 'images/example.png',
+//           vibrate: [100, 50, 100],
+//           data: {
+//             dateOfArrival: Date.now(),
+//             primaryKey: 1
+//           }
+//         };
+//         reg.showNotification('Hello world!', options);
+//       });
+//     }
+//   }
+//   displayNotification();
